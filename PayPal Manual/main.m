499a274f819fff2f28c274edd09131b25434534f
leonelgpr33 <leonel.perez@mrci.com.mx>
2016-01-03 13:03:25 -0600

    Initial Commit

---
+++ PayPal Manual/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayPal Manual
+//
+//  Created by LI Leonel G. Pérez Ramos on 03/01/16.
+//  Copyright © 2016 LI Leonel G. Pérez Ramos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
